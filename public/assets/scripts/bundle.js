ebcf1d529e4c64b9380d3a9c0c69977838bc34ac
Vishen Gounden <vgounden@gmail.com>
2021-03-20 23:46:43 +0200

    remove public

--- public/assets/scripts/bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";(()=>{const e=document.querySelector(".bacon");fetch("https://baconipsum.com/api/?type=all-meat&paras=3").then((e=>e.json())).then((t=>{const c=t.reduce(((e,t)=>e+`<p>${t}</p>`),"");e.innerHTML=c})).catch((t=>e.innerHTML=t))})()})();
